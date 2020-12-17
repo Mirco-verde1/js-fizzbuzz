388817f5949405036c07f23b3b22a7be0ca6a779
Mirco <verderosamircowork@gmail.com>
2020-12-17 17:07:09 +0100

    Uniti i multipli di 3 e 5 in un unica dicitura

--- js/script.js
+++ js/script.js
@@ -1,17 +1,14 @@
 // Creo una lista da 1 a 100 che mi permetta di trovare i multipli di 3 e di 5 e i multipli in comune;
 
-for(var i = 1; i < 101; i++) // inizializzo il contatore
+for(var i = 0; i < 101; i++) // inizializzo il contatore
 
-  if (i % 3 === 0) {
-     console.log('Fizz ' + i) // multipli di 3
+  if (i % 3 === 0  && i % 5 === 0) {
+     console.log('Fizzbuzz ' + i) // multipli di 3 e 5
   }
 
-   else if ( i % 5 === 0){     // multipli di 5
-    console.log('Buzz ' + i)
+   else if ( i % 3 === 0){     //  solo multipli di 3
+    console.log('fizz '+ i)
   }
-else {
-
+else if(i % 5 === 0){          // solo multipli di 5
+console.log('Buzz' + i);
 };
-
-if (i % 3 === 0 && i % 5 === 0){
-console.log('Fizzbuzz' + i)};
